1f5a57f653d46fe45ff10201e2c78005dfff5fe7
MAMATOV AZIZKHON <mamatov717@gmail.com>
2022-09-26 10:19:23 +0900

    Shared api
    Shared Api for whole webpage

--- src/shared/axios.ts
+++ src/shared/axios.ts
@@ -1,2 +1,13 @@
 import axios from "axios"
-import 
+import { Api_Url } from "./const"
+
+const instance=axios.create({
+    baseURL:Api_Url,
+    params:{
+        api_key:"84d23d1ea2504e1e6b171a06af819654",
+    },
+});
+
+
+export default instance;
+
---
+++ src/shared/const.ts
@@ -0,0 +1,11 @@
+export const Api_Url="https://api.themoviedb.org/3"
+export const Image_Url="https://image.tmdb.org/t/p"
+
+export const Supported_Query={
+    genre:[],
+    sort_by:[],
+    minRuntime:[],
+    maxRuntime:[],
+    from:[],
+    to:[],
+};
